2686fccc519a98af02864b9e98545624d99a4147
Yogesh <yogeshk25it@srishakthi.ac.in>
2026-03-12 15:57:50 +0530

    fixed error in import line

--- el_gurrero/lib/main.dart
+++ el_gurrero/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_game/el_gurrero.dart';
+import 'el_gurrero.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
